29e00bb2e080b08ce9757dbe8f733d08cf2166c7
Rohit Sharma <rsharm8986@gmail.com>
2023-06-23 14:11:22 +0530

    portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,13 +13,25 @@ const Portfolio = () => {
     return (
         <>
             <div className="container portfolio-page">
-                <h1 className="page-title">
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={"Portfolio".split("")}
-                        idx={15}
-                    />
-                </h1>
+                <div className="text-zone">
+
+                    <h1 >
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={"Portfolio".split("")}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>
+                        I am a passionate frontend developer with 2 years of experience in the field. I specialize in creating dynamic and responsive user interfaces using technologies such as React, Bootstrap, HTML, CSS, JavaScript, Material UI, and jQuery. I am dedicated to delivering high-quality code and creating engaging web experiences.
+                    </p>
+                    <p>
+                        In addition to my technical skills, I have a strong eye for design and a deep understanding of user experience principles. I am constantly staying updated with the latest industry trends and technologies to ensure that I am delivering modern and cutting-edge solutions.
+                    </p>
+                    <p>
+                        I am always excited to take on new challenges and contribute my expertise to create exceptional web experiences. Feel free to explore my portfolio to see some of the projects I have worked on. If you have any questions or would like to discuss a potential collaboration, please don't hesitate to get in touch.
+                    </p>
+                </div>
 
             </div>
             <Loader type="pacman" />
